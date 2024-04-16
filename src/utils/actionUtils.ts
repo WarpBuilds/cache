fd061fb6dd07dab9d32545cfd7c5059452b00126
Prajjwal <prajjwaldimri@hotmail.com>
2024-04-16 10:41:07 +0530

    chore: changes message to point to warpbuild status

--- src/utils/actionUtils.ts
+++ src/utils/actionUtils.ts
@@ -80,7 +80,7 @@ Otherwise please upgrade to GHES version >= 3.5 and If you are also using Github
     }
 
     logWarning(
-        "An internal error has occurred in cache backend. Please check https://www.githubstatus.com/ for any ongoing issue in actions."
+        "An internal error has occurred in cache backend. Please check https://status.warpbuild.com/ for any ongoing issue in actions."
     );
     return false;
 }
